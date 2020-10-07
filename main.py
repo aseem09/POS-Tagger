2de9630e7c3d3636bcc9acdf8ca117d2df38816e
aseem09 <aseemverma999@gmail.com>
2020-10-07 21:13:48 +0530

    Add graphs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from reader import parse_data, list_to_freq_dict, parse_data_get_attrib
-from collections import Counter 
+from collections import Counter
+import matplotlib.pyplot as plt
+import pandas as pd
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -13,6 +15,11 @@ print('Top 10 Words are: ')
 for i in top_words:
     print(i[0]," :",i[1]," ")
 
+top_words = k1.most_common(25)
+keys, values = [i[0] for i in top_words],[i[1] for i in top_words]
+plt.pie(values, labels=keys)
+plt.show()
+
 print('\n\n')
 
 tag_list = parse_data_get_attrib(fileName, 'c5')
@@ -22,6 +29,11 @@ top_tags = k1.most_common(10)
 print('Top 10 Tags are: ')
 for i in top_tags:
     print(i[0]," :",i[1]," ")
+
+top_tags = k1.most_common(25)
+keys, values = [i[0] for i in top_tags],[i[1] for i in top_tags]
+plt.pie(values, labels=keys)
+plt.show()
 
 print('\n\n')
 
